8e87739516d8bdd0a8d055e1fb5eef7eaf460ba2
Jos Visser <j.visser@coolblue.nl>
2020-01-01 14:48:21 +0100

    Clean-up sample tests and fix test dcu path

--- source/Test/AoC2019Tests.dpr
+++ source/Test/AoC2019Tests.dpr
@@ -17,7 +17,6 @@ program AoC2019Tests;
 uses
   DUnitX.TestFramework,
   TestInsight.DUnitX,
-  Sample.Tests in 'Sample.Tests.pas',
   Module.Fuel.Tests in 'Module.Fuel.Tests.pas',
   Puzzle.Y2019.Tests in 'Puzzle.Y2019.Tests.pas',
   Aoc.TestCase.Attribute in 'Aoc.TestCase.Attribute.pas',
